66387e53747f0832d1a64fa0c723a78ec4c350bc
ZachPeterson22 <43228351+ZachPeterson22@users.noreply.github.com>
2025-01-09 13:01:02 -0600

    Updated script.js

--- script.js
+++ script.js
@@ -5,12 +5,20 @@ const imgUrl = document.getElementById("imgInput");
 
 addImg.addEventListener("click", function() {
     for (let i = 0; i < 6; i++) {
+        const mainContainer = document.createElement("div");
+        const backgroundContainer = document.createElement("p");
+        const imageContainer = document.createElement("div");
+        const bckgrnd = document.createElement("img");
+        bckgrnd.src = "morablue.png";
         const picture = document.createElement("img");
-        // comment line 10 out and hardcode a img file if the url doesnt work
         picture.src = imgUrl.value;
-        printArea.appendChild(picture);
+        printArea.appendChild(mainContainer);
+        mainContainer.appendChild(backgroundContainer);
+        backgroundContainer.appendChild(bckgrnd);
+        mainContainer.appendChild(imageContainer);
+        imageContainer.appendChild(picture);
     }
-   
+    
 });
 
 printBtn.addEventListener("click", function() {
